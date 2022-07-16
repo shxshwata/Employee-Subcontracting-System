7d1d06c84f12a6cc761005a6c09868e7ca1b9cde
Shashwata Ghosh <41067771+shxshwata@users.noreply.github.com>
2022-07-16 22:05:37 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -4,3 +4,9 @@ mysql> create table MainTable
 -> EmpPost varchar(70),
 -> EmpLocation varchar(70),
 -> EmpSalary integer);
+mysql> create table Applicants
+-> (UID varchar(10) NOT NULL PRIMARY KEY, -> AppName varchar(70) NOT NULL,
+-> AppAge integer,
+-> AppLocation varchar(70), -> AppExpSalary integer);
+mysql> create table Status
+-> (UID varchar(10) NOT NULL PRIMARY KEY, -> EmpStatus varchar(70) NOT NULL);
