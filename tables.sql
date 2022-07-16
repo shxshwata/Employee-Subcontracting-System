221ef118542fb507f05bb382c268f1299b15d017
Shashwata Ghosh <41067771+shxshwata@users.noreply.github.com>
2022-07-16 22:04:50 +0530

    Create tables.sql

---
+++ tables.sql
@@ -0,0 +1,6 @@
+mysql> create table MainTable
+-> (UID varchar(10) NOT NULL PRIMARY KEY, -> EmpName varchar(70) NOT NULL,
+-> EmpAge integer,
+-> EmpPost varchar(70),
+-> EmpLocation varchar(70),
+-> EmpSalary integer);
